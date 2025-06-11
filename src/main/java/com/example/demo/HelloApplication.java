1f5f63fc2e64538d812f7f4e59909cb018cedd32
Muhammad-Sareer25 <sareer2002@gmail.com>
2025-06-11 10:55:51 +0500

    Commit done DB connection

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -17,6 +17,7 @@ public class HelloApplication extends Application {
 
         stage.show();
         stage.setResizable(false);
+        System.out.println("HELlO");
     }
 
     public static void main(String[] args) {
